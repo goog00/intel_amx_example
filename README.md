915c92e1227aa2c627d912c862dc1601e5c17d31
sunteng <steng2009@163.com>
2025-04-05 20:25:57 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 intel_amx_example
+
+### 编译命令
+
+clang++ -O3  -march=native -mamx-tile -mamx-int8 -fno-strict-aliasing matrix_mul_amx.cpp -o matrix_mul_amx
--- src/matrix_mul_amx.cpp
+++ src/matrix_mul_amx.cpp
@@ -5,8 +5,15 @@
 #include <iostream>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <cassert>
+#include <memory>
+#include <chrono>
 
-template <typename DataType> 
+#include <iomanip>
+
+
+
+template <typename DataType>
 class Matrix {
 
 private:
@@ -25,6 +32,8 @@ public:
   int Rows() const { return rows; }
   int Cols() const { return cols; }
 
+  int Size() const { return rows * cols; }
+
   // 用于初始化
   void Fill(DataType value) {
     for (int i = 0; i < rows * cols; ++i) {
@@ -33,7 +42,7 @@ public:
   }
 
   // 打印矩阵
-  void Print() const {
+  void Print_t() const {
     for (int i = 0; i < rows; ++i) {
       for (int j = 0; j < cols; ++j) {
         std::cout << static_cast<int>(data[i * cols + j]) << " ";
@@ -53,35 +62,41 @@ struct __tile_config {
   uint8_t rows[16];   // 每个 tile 的行数
 };
 
-template <typename InputType, typename OutputType> 
+template <typename InputType, typename OutputType>
 class IntelAmxMatrixMultiply {
+
 private:
+
+  IntelAmxMatrixMultiply() = default;
+
+  void InitTileConfig() {
+    __tile_config tileinfo{};
+    tileinfo.palette_id = 1;
+    tileinfo.start_row = 0;
+    for (int i = 0; i < 8; ++i) {
+      tileinfo.colsb[i] = COLSB;
+      tileinfo.rows[i] = ROWS;
+    }
+    _tile_loadconfig(&tileinfo);
+  }
+
   int ARCH_REQ_XCOMP_PERM = 0x1023;
   int XFEATURE_XTILEDATA = 18;
   int ROWS = 16;
   int COLSB = 64;
 
-  void InitTileConfig(__tile_config *tileinfo) {
-
-    int i;
-    tileinfo->palette_id = 1;
-    tileinfo->start_row = 0;
-    for (i = 0; i < 8; ++i) {
-      tileinfo->colsb[i] = COLSB;
-      tileinfo->rows[i] = ROWS;
-    }
-
-    _tile_loadconfig(tileinfo);
-  }
 
 public:
 
+  static IntelAmxMatrixMultiply Create() {
+    IntelAmxMatrixMultiply self;
+    self.SetTileDataUse();
+    self.InitTileConfig();
+    return self;
+  }
+
   void MatrixMultiply(Matrix<InputType> &A, Matrix<InputType> &B,
                            Matrix<OutputType> &C) {
-
-    __tile_config tile_data = {0};
-
-    InitTileConfig(&tile_data);
 
     _tile_loadd(2, A.Data(), A.Stride());
     _tile_loadd(3, B.Data(), B.Stride());
@@ -93,9 +108,8 @@ public:
   }
 
   bool SetTileDataUse() {
-    if (syscall(SYS_arch_prctl, ARCH_REQ_XCOMP_PERM, XFEATURE_XTILEDATA)) {
-      return false;
-    } 
+    auto res = syscall(SYS_arch_prctl, ARCH_REQ_XCOMP_PERM, XFEATURE_XTILEDATA);
+    assert(!res && "fail:Invoke syscall to set ARCH_SET_STATE_USE ");
     return true;
   }
 
@@ -109,31 +123,43 @@ public:
 int main() {
   // 创建矩阵
   Matrix<int8_t> A(16, 64);
-  Matrix<int8_t> B(16, 64);
+  Matrix<int8_t> B(64, 16);
   Matrix<int32_t> C(16, 16);
 
   // 初始化矩阵
   A.Fill(2);
-  A.Print();
+  // A.Print();
   B.Fill(2);
-  B.Print();
+  // B.Print();
   C.Fill(0);
-  C.Print();
+  // C.Print();
 
   // 执行乘法
-  IntelAmxMatrixMultiply<int8_t, int32_t> multiply;
-  if (!multiply.SetTileDataUse()) {
-    exit(-1);
+  auto multiply = IntelAmxMatrixMultiply<int8_t, int32_t>::Create();
+
+  int k = 100000;
+  auto t0 = std::chrono::high_resolution_clock::now();
+  for(int i = 0 ; i < k; i++){
+    multiply.MatrixMultiply(A, B, C);
   }
-  multiply.MatrixMultiply(A, B, C);
+  auto t1 = std::chrono::high_resolution_clock::now();
+
   multiply.TileRelease();
-  // 打印结果（简单验证）
-  for (int i = 0; i < C.Rows(); ++i) {
-    for (int j = 0; j < C.Cols(); ++j) {
-      std::cout << C.Data()[i * C.Cols() + j] << " ";
-    }
-    std::cout << "\n";
-  }
+
+  // // 打印结果（简单验证）
+  // for (int i = 0; i < C.Rows(); ++i) {
+  //   for (int j = 0; j < C.Cols(); ++j) {
+  //     std::cout << C.Data()[i * C.Cols() + j] << " ";
+  //   }
+  //   std::cout << "\n";
+  // }
+
+  auto cost_time = static_cast<double>((t1 - t0).count());
+  auto ops_per_matmul = int64_t(A.Rows()) * A.Cols() * B.Cols() * 2;
+  auto items  = static_cast<double>(ops_per_matmul * k);
+  auto gflops = items / cost_time ;
+  std::cout <<"循环次数: " << k << "\n";
+  std::cout <<"Intel Amx cost time:" << cost_time / 1e9 <<"s, GFLOPS: " << std::fixed << std::setprecision(4) << gflops << "gflops" << "\n";
 
   return 0;
 }
